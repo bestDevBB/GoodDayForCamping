582768ac63f139be9dcb55ce58430716566a551b
Boram Lee <cnn93@naver.com>
2022-02-07 00:52:32 +0900

    first commit

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -14,4 +14,11 @@ function login() {
         id: id.value,
         password: password.value
     };
-};
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json" // 내가 보내는 데이터의 타입
+        }, // JSON 데이터라고 알려줌
+        body: JSON.stringify(req) // req를 문자열로 바꿔줌
+    }); // 서버에 전달
+}; // 이러한 데이터를 서버에서 받으려면 /login 경로, POST 메서드로 데이터를 받을 수 있는 API가 마련되어있어야 함
