4fd8d774698f9955e0bb64a754a00e14b74957d6
Boram Lee <cnn93@naver.com>
2022-02-07 00:41:43 +0900

    first commit

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+// DOM
+const id = document.querySelector("#id"), // <input id = "id" .../>
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button");
+
+//console.log(id); // null, #id를 가져오기 전에 console가 먼저 실행되서 null이 뜸
+
+loginBtn.addEventListener("click", login); // login()
+
+function login() {
+    const req = {
+        id: id.value,
+        password: password.value
+    };
+};
