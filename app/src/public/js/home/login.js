8e33ba429b152e4b9212d2fceac186ad0ef8bee5
Boram Lee <cnn93@naver.com>
2022-02-08 01:03:34 +0900

    회원가입 요청 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -3,7 +3,7 @@
 // DOM
 const id = document.querySelector("#id"), // <input id = "id" .../>
     password = document.querySelector("#password"),
-    loginBtn = document.querySelector("button");
+    loginBtn = document.querySelector("#button");
 
 //console.log(id); // null, #id를 가져오기 전에 console가 먼저 실행되서 null이 뜸
 
---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// DOM
+const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
+    password = document.querySelector("#password"),
+    confirmPassword = document.querySelector("#confirm-password"),
+    registerBtn = document.querySelector("#button");
+
+
+
+registerBtn.addEventListener("click", register);
+
+function register() {
+    const req = {
+        id: id.value,
+        name: name.value,
+        password: password.value,
+        confirmPassword: confirmPassword.value
+    };
+
+    fetch("/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req) // SIGN UP 버튼이 눌리면 id, name 등 해당하는 데이터들을 이 body에 담아서 전달 -> POST로 요청
+    })
+    .then((res) => res.json()) // 서버로부터 응답이 오면 json 메소드를 호출해서 서버에 응답이 다 받아지는 순간 Promise 객체를 반환
+    .then((res) => { // Promise 객체로 반환을 했으니까 then으로 접근해서 res로 접근
+        if(res.success) {
+            location.href = "/login";
+        } else {
+            alert(res.msg);
+        };
+    })
+    .catch((err) => {
+        console.error(new Error("회원가입 중 에러 발생"));
+    });
+};
