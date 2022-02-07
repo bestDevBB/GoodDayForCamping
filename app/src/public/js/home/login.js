66dfcadd7b2dd5c848793e7e69c7f1f9046c967a
Boram Lee <cnn93@naver.com>
2022-02-08 00:30:07 +0900

    로그인화면 꾸미기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -40,6 +40,5 @@ function login() {
 기본 res의 반환 값은 Response 스트림인데
 ".json()" 메서드를 통해 Response(응답) 스트림을 읽을 수 있다.
 Response는 데이터가 모두 받아진 상태가 아니다.
-".json()"으로 Response 스트림을 가져와 완료될 떄까지 읽는다.
-다 읽은 body의 텍스트를 Promise 형태로 반
-환한다. */
+".json()"으로 Response 스트림을 가져와 완료될 때까지 읽는다.
+다 읽은 body의 텍스트를 Promise 형태로 반환한다. */
